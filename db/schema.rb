54ab6ca108e7eac81d41e30a4ac5bd4dfc6c7ddb
BertinManoelBr <manoelcarlobertin@outlook.com>
2025-06-26 19:57:09 -0300

    criada a coluna de item da tabela tarefas

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_06_26_224706) do
+ActiveRecord::Schema[8.0].define(version: 2025_06_26_225451) do
+  create_table "todo_items", force: :cascade do |t|
+    t.string "description"
+    t.datetime "completed_at"
+    t.integer "todo_list_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["todo_list_id"], name: "index_todo_items_on_todo_list_id"
+  end
+
+  create_table "todo_lists", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_todo_lists_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -22,4 +40,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_06_26_224706) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
+  add_foreign_key "todo_items", "todo_lists"
+  add_foreign_key "todo_lists", "users"
 end
